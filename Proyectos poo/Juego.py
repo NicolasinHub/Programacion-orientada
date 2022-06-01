1b31e9391de651f3d98180c0a92fa27cf84c1080
Nicolas Muñoz <nimunozr@correo.udistrital.edu.co>
2022-06-01 14:19:50 -0500

    Juego, ¿error?

--- Proyectos poo/Juego.py
+++ Proyectos poo/Juego.py
@@ -2,12 +2,15 @@ from re import A
 import pygame
 import random
 
+Tamaño=1200,800
 Blanco=255,255,255
 Negro=0,0,0
+Posicionx=600
+Posiciony=400
 
 pygame.init()
 
-screen=pygame.display.set_mode((1200,800))
+screen=pygame.display.set_mode(Tamaño)
 
 running=True
 
@@ -15,11 +18,17 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-    
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                Posicionx = Posicionx+1
+            if event.key == pygame.K_d:
+                Posicionx = Posicionx-1
+            if event.key == pygame.K_w:
+                Posicionx = Posiciony-1
+            if event.key == pygame.K_s:
+                Posicionx = Posiciony+1
+            
     pygame.display.update()
-    pygame.draw.circle(screen, Blanco, (600,400),random.randrange(10,50))
-    pygame.display.flip()
-    
-    keyboard=pygame.key.get_pressed()
-    if keyboard[pygame.K_a]:
-        
+    pygame.draw.circle(screen, Blanco, (Posicionx,Posiciony),random.randrange(10,50))
+    pygame.display.flip()   
