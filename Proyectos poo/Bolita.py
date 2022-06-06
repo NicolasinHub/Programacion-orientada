cdee755c63f6d68b9bd9e24981b83e141b7308f1
Nicolas Muñoz <nimunozr@correo.udistrital.edu.co>
2022-06-06 06:43:26 -0500

    Iniciar pygame, pantalla y cerrar pantalla

--- Proyectos poo/Bolita.py
+++ Proyectos poo/Bolita.py
@@ -6,3 +6,14 @@ Negro=0,0,0
 Posicionx=400
 Posiciony=300
 BallSize=20
+
+pygame.init()
+
+screen=pygame.display.set_mode(ScreenSize)
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            quit()
+            
