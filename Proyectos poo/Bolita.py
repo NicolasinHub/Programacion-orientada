b39ac1459b448974f410534ad185bfd07daad820
Nicolas Muñoz <nimunozr@correo.udistrital.edu.co>
2022-06-06 10:16:24 -0500

    La bola se mueve en intervalos

--- Proyectos poo/Bolita.py
+++ Proyectos poo/Bolita.py
@@ -1,13 +1,12 @@
 
+import sys, pygame
 from turtle import Screen, circle
 import pygame
 
 ScreenSize=800,600
-BallSize=30
+width, heigth=800, 600
 Blanco=255,255,255
 Negro=0,0,0
-Posicionx=400
-Posiciony=300
 Posicion=[1, 1]
 
 pygame.init()
@@ -15,19 +14,24 @@ pygame.init()
 screen=pygame.display.set_mode(ScreenSize)
 clock=pygame.time.Clock()
 
+Ball=pygame.image.load("Bola.jpg")
+Ballrect = Ball.get_rect()
+
 while True:
+    #pygame.time.delay(2)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             quit()
+        Ballrect=Ballrect.move(Posicion)
+    if Ballrect.left < 0 or Ballrect.right > width:
+        Posicion[0]= -Posicion[0]
+    if Ballrect.top < 0 or Ballrect.bottom > heigth:
+        Posicion[1]= -Posicion[1]
         
     
-    if (BallSize-Posicionx) < Posicionx:
-        Posicion[0]= -Posicion[0]
-    
-    Posicion=
     pygame.display.update()
     screen.fill(Blanco)
-    pygame.draw.circle(screen, Negro, (Posicionx,Posiciony), (BallSize))
+    screen.blit(Ball, Ballrect)
     pygame.display.flip()  
-    clock.tick(20)
+    clock.tick(150)
