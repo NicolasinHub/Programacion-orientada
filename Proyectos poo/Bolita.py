2bef4d39af8b2c43f6e3c237bd2a40f480e75203
Nicolas Muñoz <nimunozr@correo.udistrital.edu.co>
2022-06-06 07:09:05 -0500

    Imagen de bola y obtener rectangulo

--- Proyectos poo/Bolita.py
+++ Proyectos poo/Bolita.py
@@ -1,4 +1,6 @@
+from turtle import Screen, circle
 import pygame
+import sys, pygame
 
 ScreenSize=800,600
 Blanco=255,255,255
@@ -6,14 +8,23 @@ Negro=0,0,0
 Posicionx=400
 Posiciony=300
 BallSize=20
+Speed=[1, 1]
 
 pygame.init()
 
 screen=pygame.display.set_mode(ScreenSize)
+clock=pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             quit()
-            
+        
+    Ball=pygame.image.load("Ball.jpg")
+    Ballrect= Ball.get_rect()
+        
+    pygame.display.update()
+    screen.fill(Blanco)
+    pygame.display.flip()  
+    clock.tick(20)
